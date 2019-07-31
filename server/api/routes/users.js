f644c02c3a22169b751a468c43352902564e4ec2
Zach Resnick <zachary.l.resnick@gmail.com>
2019-07-31 17:56:19 -0400

    got the cart api route to work

--- server/api/routes/users.js
+++ server/api/routes/users.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
-const {User} = require('../../db/models')
+const {User, Cart, CartItem, Product} = require('../../db/models')
+const utils = require('./utils')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
+router.get('/', utils.adminGateway, async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
@@ -11,6 +12,21 @@ router.get('/', async (req, res, next) => {
       attributes: ['id', 'email']
     })
     res.json(users)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id/cart', utils.userGateway, async (req, res, next) => {
+  try {
+    const cart = await Cart.findOne({
+      include: [{model: Product}],
+      where: {
+        userId: req.params.id,
+        active: true
+      }
+    })
+    res.json(cart)
   } catch (err) {
     next(err)
   }
