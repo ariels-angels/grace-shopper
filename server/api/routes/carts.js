9dcc35f3721d6eadbb1494ac62faedd7029d22c5
Zach Resnick <zachary.l.resnick@gmail.com>
2019-08-02 10:27:49 -0400

    Updated cart api to allow double-adding of the same product

--- server/api/routes/carts.js
+++ server/api/routes/carts.js
@@ -58,7 +58,7 @@ router.get('/all/:id', utils.adminGateway, async (req, res, next) => {
   }
 })
 
-//this route can add items to the active cart
+//this route can add items to the active cart.  CHANGE THIS SO THAT ITS FINDORCREATE AND MAKE IT WORK
 router.post('/active', async (req, res, next) => {
   try {
     const cart = await Cart.findOne({
@@ -67,20 +67,33 @@ router.post('/active', async (req, res, next) => {
         active: true
       }
     })
-    const newItem = await CartItem.create({
-      cartId: cart.id,
-      productId: req.body.productId,
-      quantity: req.body.quantity
-      // price: req.body.productPrice
+    const existingItem = await CartItem.findOne({
+      where: {
+        cartId: cart.id,
+        productId: req.body.productId
+      }
     })
-    res.json(newItem)
+    if (!existingItem) {
+      const newItem = await CartItem.create({
+        cartId: cart.id,
+        productId: req.body.productId,
+        quantity: req.body.quantity
+        // price: req.body.productPrice
+      })
+      res.json(newItem)
+    } else {
+      await existingItem.update({
+        quantity: existingItem.quantity + req.body.quantity
+      })
+      res.json(existingItem)
+    }
   } catch (error) {
     next(error)
   }
 })
 
 //this route will delete an item from a cart
-router.put('/active/delete/', async (req, res, next) => {
+router.put('/active/delete', async (req, res, next) => {
   try {
     console.log('req.body>>>>', req.body)
     const cart = await Cart.findOne({
@@ -104,6 +117,30 @@ router.put('/active/delete/', async (req, res, next) => {
       }
     })
     res.json(updatedCart)
+  } catch (error) {
+    next(error)
+  }
+})
+
+//CREATE A ROUTE.PUT TO /ACTIVE THAT WILL RECEIVE REQ.BODY.ID (PRODUCTID) AND REQ.BODY.QUANTITY
+router.put('/active', async (req, res, next) => {
+  try {
+    const cart = await Cart.findOne({
+      where: {
+        userId: req.user.id,
+        active: true
+      }
+    })
+    const existingItem = await CartItem.findOne({
+      where: {
+        cartId: cart.id,
+        productId: req.body.productId
+      }
+    })
+    await existingItem.update({
+      quantity: req.body.quantity
+    })
+    res.json(existingItem)
   } catch (error) {
     next(error)
   }
